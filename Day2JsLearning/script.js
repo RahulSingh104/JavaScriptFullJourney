c77c3c1883ae3b9bcfcf178a56860ec4ff4b296c
RahulSingh104 <rahulsingh96287@gmail.com>
2026-03-20 22:06:49 +0530

    First Day of learning

---
+++ Day2JsLearning/script.js
@@ -0,0 +1,37 @@
+// Step 1 - to Write a function 
+// what is fnc
+function abcd (){
+
+}
+
+// why fnc
+function greet (){
+    console.log('hello world');
+
+}
+
+greet();  //iss matlab func ko chalo or call karna fnc ko 
+greet();
+greet();
+greet();
+
+
+function greetings(val){
+    console.log(val);
+
+}
+
+greetings("hey");
+greetings("hello");
+greetings("Rahul");
+
+
+function bcd(a,b,c,d){
+    console.log(a+b+c+d);
+
+}
+
+bcd(1,50,84,96)
+
+
+
