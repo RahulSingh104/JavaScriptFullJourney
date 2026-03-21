82eb0022cf45db5d1dd0b0c9aee690c2cdb4fb13
RahulSingh104 <rahulsingh96287@gmail.com>
2026-03-22 00:04:06 +0530

    Task of function logic

--- Day3/script.js
+++ Day3/script.js
@@ -47,3 +47,129 @@ var objj = {
     isMarried: false,
 }
 objj.email
+
+
+
+// Problem 
+// User adds Products -> calculate total -> apply discount
+
+//  this code is bad code according to the SOlid principle
+function cart1 (data){
+    let total = 0;
+    for(let item of data){
+        total +=item.price *item.qty;
+
+    }
+    if(total > 1000){
+        total = total - total*0.1;
+    }
+    return total;
+}
+
+
+// good code 
+
+const cart = [
+    {name: "Shirt", price:500,qty:2},
+    {name: "Shoes", price:800,qty:1},
+];
+
+
+function calculateItemTotal(item){
+    return item.price * item.qty;
+}
+
+function calculateCartTotal(cart){
+    return cart.reduce((total,item)=> total + calculateItemTotal(item),0);
+}
+ function applyDiscount(total){
+    if(total > 1000){
+        return total * 0.9;
+    }
+    return total;
+
+ }
+ const total = calculateCartTotal(cart);
+ const finalPrice = applyDiscount(total);
+
+ console.log(finalPrice);
+
+
+//  User Management System 
+// 🎯 Problem:
+// Get user full name
+// Filter active users
+
+const users = [
+    {FN:"Rahul",LN:"Singh",isAcitve:true},
+    {FN:"AnnuRadha",LN:"Patel",isActive:false},
+];
+
+function getFn(user){
+    return `${user.FN} ${user.LN}`;
+}
+function isAcitveUser(user){
+    return user.isAcitve;
+
+}
+
+function getActiveUsers(users){
+    return users.filter(isAcitveUser);
+}
+
+const acitveUsers = getActiveUsers(users);
+
+acitveUsers.forEach(user =>{
+    console.log(getFn(user))
+})
+
+
+
+const products = [
+    {name:"Phone",price:20000,inStock:true},
+    {name:"laptop",price:50000,inStock:false},
+    {name:"Tablet",price:15000,inStock:true}
+];
+
+function getProductTotal(product){
+    return `${product.name}: ${product.price}`
+}
+
+function isCheapAndInStock(product){
+    return product.inStock===true && product.price < 30000;
+}
+function getFilterProducts(products){
+    return products.filter(isCheapAndInStock)
+}
+
+const  StockProduct = getFilterProducts(products);
+
+StockProduct.forEach(product =>{
+    console.log(getProductTotal(product))
+})
+
+
+
+const students  = [
+    {name:"Rahul", marks:85},
+    {name:"Annuraddha p", marks: 80},
+    {name:"Neha",marks:70},
+];
+
+function studentMarksTotal(student){
+    return `${student.name} ${student.marks}`
+}
+
+function PassStudent (student){
+    return student.marks>=50
+}
+
+function passedStudentsList(studentsArray){
+    return studentsArray.filter(PassStudent)
+}
+
+const passedOnes = passedStudentsList(students)
+
+passedOnes.forEach(student =>{
+    console.log(studentMarksTotal(student))
+})
